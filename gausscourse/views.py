ab65997c78fcfc3456795300fe4fc3360f4d9dc6
MorriganR <dasha@ddyka.com>
2021-03-05 21:41:55 +0200

    Create simple table from db on main site page

--- gausscourse/views.py
+++ gausscourse/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Course
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    """return HttpResponse("Hello, world. You're at the polls index.")"""
+    course_list = Course.objects.all()
+    context = {'course_list': course_list}
+    return render(request, 'gausscourse/index.html', context)
--- gaussscore/urls.py
+++ gaussscore/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+    path('gausscourse/', include('gausscourse.urls')),
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gausscourse.urls')),
