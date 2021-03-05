7aef245d7fbc418ab7d38b7d95021b53e72ae70b
MorriganR <dasha@ddyka.com>
2021-03-05 21:40:31 +0200

    Rewrite __str__(self) for good look in adminka

--- gausscourse/models.py
+++ gausscourse/models.py
@@ -6,9 +6,13 @@ class Course(models.Model):
     name = models.CharField(max_length=512)
     start_date = models.DateTimeField('date started')
     finish_date = models.DateTimeField('date finished')
+    def __str__(self):
+        return self.name
 
 class CourseGroup(models.Model):
     name = models.CharField(max_length=512)
     curator = models.CharField(max_length=512)
     quantity_st = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name + " - " + self.curator
