3797cc948b9b8feee70a6de4170f49da3653b876
MorriganR <dasha@ddyka.com>
2021-03-17 13:53:54 +0200

    Test page that explains render context

--- gausscourse/urls.py
+++ gausscourse/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('test/', views.test, name='test'),
     path('', views.index, name='index'),
 ]
--- gausscourse/views.py
+++ gausscourse/views.py
@@ -11,3 +11,11 @@ def index(request):
     course_list = Course.objects.all()
     context = {'course_list': course_list}
     return render(request, 'gausscourse/index.html', context)
+
+def test(request):
+    """return HttpResponse("Hello, world. You're at the polls index.")"""
+    course_list = [{'name':'name', 'start_date':'start_date', "finish_date":"finish_date" },
+                   {'name':'sfjh', 'start_date':'rrrrrrrrrr', "finish_date":"dfjdjdyjdyj" },
+                   {'name':'gjfj', 'start_date':'ggggggffff', "finish_date":"cvmn4565676" }]
+    context = {'course_list': course_list}
+    return render(request, 'gausscourse/test.html', context)
