1045aed6bff5edb9206ec897faaa1f403e5879d6
MorriganR <dasha@ddyka.com>
2021-04-20 15:53:43 +0200

    add mpld3 & 15 lines of code (venvdev) DjangoCoursework>pip install mpld3 (venvdev) DjangoCoursework>python -m pip freeze > requirements.txt

--- gausscourse/views.py
+++ gausscourse/views.py
@@ -21,9 +21,22 @@ def index(request):
     return render(request, 'gausscourse/index.html', context)
 
 def test(request):
+    from matplotlib.pyplot import figure, title, bar
+    import numpy as np
+    import mpld3
+
+    mpl_figure = figure(figsize=(9, 4))
+    xvalues = range(100)  # the x locations for the groups
+    yvalues = np.random.random_sample(100)
+
+    width = 0.9  # the width of the bars
+    title('Еволюційні обчислення')
+    bar(xvalues, yvalues, width)
+    fig_html = mpld3.fig_to_html(mpl_figure)
+
     """return HttpResponse("Hello, world. You're at the polls index.")"""
     course_list = [{'name':'name', 'start_date':'start_date', "finish_date":"finish_date" },
                    {'name':'sfjh', 'start_date':'rrrrrrrrrr', "finish_date":"dfjdjdyjdyj" },
                    {'name':'gjfj', 'start_date':'ggggggffff', "finish_date":"cvmn4565676" }]
-    context = {'course_list': course_list}
+    context = {'figure': fig_html, 'course_list': course_list}
     return render(request, 'gausscourse/test.html', context)
