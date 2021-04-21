1f78ce146bc4097788113e38621f64f92e413928
MorriganR <dasha@ddyka.com>
2021-04-21 13:03:07 +0200

    matplotlib - Play around Histogram (fig2_html)

--- gausscourse/views.py
+++ gausscourse/views.py
@@ -21,22 +21,46 @@ def index(request):
     return render(request, 'gausscourse/index.html', context)
 
 def test(request):
+    import matplotlib.pyplot as plt
     from matplotlib.pyplot import figure, title, bar
     import numpy as np
     import mpld3
 
+  # Play around Bar (fig_html)
     mpl_figure = figure(figsize=(9, 4))
     xvalues = range(100)  # the x locations for the groups
     yvalues = np.random.random_sample(100)
-
     width = 0.9  # the width of the bars
     title('Еволюційні обчислення')
     bar(xvalues, yvalues, width)
     fig_html = mpld3.fig_to_html(mpl_figure)
+  # End Bar example
+
+  # Play around Histogram (fig2_html)
+    # example data
+    mu, sigma = 85, 10 # mean and standard deviation of distribution
+    x = mu + sigma * np.random.randn(110)
+    x = x[(x > 0) & (x < 100)] # truncate to a reasonable range
+    #num_bins = round( x.size / 5 )
+    fig, ax = plt.subplots(figsize=(9, 4))
+    ax.set_xlim(45, 100)
+    # the histogram of the data
+    #n, bins, patches = ax.hist(x, num_bins, width=width, density=True)
+    n, bins, patches = ax.hist(x, density=True)
+    # add a 'best fit' line
+    x2 = np.arange(35, 110)
+    y2 = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+        np.exp(-0.5 * (1 / sigma * (x2 - mu))**2))
+    ax.plot(x2, y2, '--')
+    ax.set_xlabel('Grade')
+    ax.set_ylabel('Density of students grades')
+    ax.set_title(f'Test Histogram: N={x.size}, NumBins={bins.size}, \u03bc={mu}, \u03C3={sigma}')
+    fig2_html = mpld3.fig_to_html(fig)
+  # End Bar example
 
     """return HttpResponse("Hello, world. You're at the polls index.")"""
     course_list = [{'name':'name', 'start_date':'start_date', "finish_date":"finish_date" },
                    {'name':'sfjh', 'start_date':'rrrrrrrrrr', "finish_date":"dfjdjdyjdyj" },
                    {'name':'gjfj', 'start_date':'ggggggffff', "finish_date":"cvmn4565676" }]
-    context = {'figure': fig_html, 'course_list': course_list}
+    context = {'figure': fig_html, 'figure2': fig2_html, 'course_list': course_list}
     return render(request, 'gausscourse/test.html', context)
